e6c2457a6d331a1509d06ba9574f389ac4538b0a
JnKindle <Jn_never2013@163.com>
2018-09-13 10:46:38 +0800

    Initial Commit

---
+++ QianChengMusic/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QianChengMusic
+//
+//  Created by Jn_Kindle on 2018/9/13.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QianChengMusic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QianChengMusic
+//
+//  Created by Jn_Kindle on 2018/9/13.
+//  Copyright © 2018年 JnKindle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
